342f0558e40e7722e5d2426e71cd8db2cb3d6478
Mili Wijeratne <tablaman@gmail.com>
2016-05-01 22:15:34 +0800

    Services
    static services

--- learning-angular2/app/app.component.ts
+++ learning-angular2/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from 'angular2/core';
 import { ProductListComponent } from './products/product-list.component';
-
+import { ProductService } from './products/product.service';
 
 @Component ({
   selector: 'pm-app',
@@ -8,7 +8,8 @@ import { ProductListComponent } from './products/product-list.component';
   <div><h1>{{ pageTitle }} </h1>
     <pm-products></pm-products>
   </div> `,
-  directives: [ProductListComponent]
+  directives: [ProductListComponent],
+  providers: [ProductService]
 })
 
 export class AppComponent {
