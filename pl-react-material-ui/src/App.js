2addb0c96c611288eb9178b09bff632d5cacf0bd
tablaman <tablaman@gmail.com>
2018-02-06 16:22:47 +0800

    Added material-ui and tested

--- pl-react-material-ui/src/App.js
+++ pl-react-material-ui/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class App extends Component {
   render() {
     return (
+      <MuiThemeProvider>
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+          <h2>Welcome to React</h2>
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <RaisedButton label="Material UI" />
       </div>
+      </MuiThemeProvider>
     );
   }
 }
