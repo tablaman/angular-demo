986c7b96fa2c1532b9d82186b5a9db487f642062
tablaman <tablaman@gmail.com>
2018-02-06 17:45:40 +0800

    Implemented most of the drawers

--- pl-react-material-ui/src/App.js
+++ pl-react-material-ui/src/App.js
@@ -1,24 +1,108 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
+
+import { withStyles } from 'material-ui/styles';
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import List from 'material-ui/List';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import Hidden from 'material-ui/Hidden';
+import Divider from 'material-ui/Divider';
+import MenuIcon from 'material-ui-icons/Menu';
+import Button from 'material-ui/Button';
+
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    height: 430,
+    marginTop: theme.spacing.unit * 3,
+    zIndex: 1,
+    overflow: 'hidden',
+  },
+  appFrame: {
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+  },
+  appBar: {
+    position: 'absolute',
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  drawerHeader: theme.mixins.toolbar,
+  drawerPaper: {
+    width: 250,
+    [theme.breakpoints.up('md')]: {
+      width: drawerWidth,
+      position: 'relative',
+      height: '100%',
+    },
+  },
+  content: {
+    backgroundColor: theme.palette.background.default,
+    width: '100%',
+    padding: theme.spacing.unit * 3,
+    height: 'calc(100% - 56px)',
+    marginTop: 56,
+    [theme.breakpoints.up('sm')]: {
+      height: 'calc(100% - 64px)',
+      marginTop: 64,
+    },
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+});
+
 
 class App extends Component {
   render() {
+    const { classes, theme } = this.props;
     return (
-      <MuiThemeProvider>
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <RaisedButton label="Material UI" />
-      </div>
-      </MuiThemeProvider>
+      <Router>
+        <MuiThemeProvider>
+          <div className="App">
+            <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Welcome to React</h2>
+            </div>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+            <Button variant="raised" color="primary">
+              Primary
+            </Button>
+          </div>
+        </MuiThemeProvider>
+        
+      </Router>
+      
+      // <div className="App">
+      //   <Header {...this.state} pageChange={this.onPageChange} />
+      //   <Route exact path="/" render={() => <Dashboard {...this.state} onPageDetailsChange={this.onPageDetailsChange} ></Dashboard>}/>
+      //   <Route exact path="/work-orders" component={() => <WorkOrders {...this.state}/>}/>
+      //   <Route exact path="/events" component={() => <Events {...this.state} pageChange={this.onPageChange}/>}/>
+      //   <Route exact path="/event/:id" component={() => <Events {...this.state} pageChange={this.onPageChange}/>}/>
+      // </div>
     );
   }
 }
