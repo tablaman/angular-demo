6c5e5b74231216a68e7f964b5f3b83850aac376d
tablaman <tablaman@gmail.com>
2018-02-08 16:37:44 +0800

    Added a responsive drawer to App for testing

--- pl-react-material-ui/src/App.js
+++ pl-react-material-ui/src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import { withStyles } from 'material-ui/styles';
+import {withStyles} from 'material-ui/styles';
 import Drawer from 'material-ui/Drawer';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
@@ -16,7 +16,6 @@ import Divider from 'material-ui/Divider';
 import MenuIcon from 'material-ui-icons/Menu';
 import Button from 'material-ui/Button';
 
-
 const drawerWidth = 240;
 
 const styles = theme => ({
@@ -25,25 +24,25 @@ const styles = theme => ({
     height: 430,
     marginTop: theme.spacing.unit * 3,
     zIndex: 1,
-    overflow: 'hidden',
+    overflow: 'hidden'
   },
   appFrame: {
     position: 'relative',
     display: 'flex',
     width: '100%',
-    height: '100%',
+    height: '100%'
   },
   appBar: {
     position: 'absolute',
     marginLeft: drawerWidth,
     [theme.breakpoints.up('md')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-    },
+      width: `calc(100% - ${drawerWidth}px)`
+    }
   },
   navIconHide: {
     [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
+      display: 'none'
+    }
   },
   drawerHeader: theme.mixins.toolbar,
   drawerPaper: {
@@ -51,8 +50,8 @@ const styles = theme => ({
     [theme.breakpoints.up('md')]: {
       width: drawerWidth,
       position: 'relative',
-      height: '100%',
-    },
+      height: '100%'
+    }
   },
   content: {
     backgroundColor: theme.palette.background.default,
@@ -62,49 +61,101 @@ const styles = theme => ({
     marginTop: 56,
     [theme.breakpoints.up('sm')]: {
       height: 'calc(100% - 64px)',
-      marginTop: 64,
-    },
+      marginTop: 64
+    }
   },
   button: {
-    margin: theme.spacing.unit,
+    margin: theme.spacing.unit
   },
   input: {
-    display: 'none',
-  },
+    display: 'none'
+  }
 });
 
-
 class App extends Component {
+  state = {
+    mobileOpen: false
+  };
+
+  handleDrawerToggle = () => {
+    this.setState({
+      mobileOpen: !this.state.mobileOpen
+    });
+  };
+
   render() {
-    const { classes, theme } = this.props;
-    return (
-      <Router>
-        <MuiThemeProvider>
-          <div className="App">
-            <div className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h2>Welcome to React</h2>
-            </div>
-            <p className="App-intro">
-              To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-            <Button variant="raised" color="primary">
-              Primary
-            </Button>
+    const {classes, theme} = this.props;
+    
+    return (<Router>
+      <MuiThemeProvider>
+        <div className={classes.root}>
+          <div className={classes.appFrame}>
+            <AppBar className={classes.appBar}>
+              <Toolbar>
+                <IconButton color="inherit" aria-label="open drawer" onClick={this.handleDrawerToggle} className={classes.navIconHide}>
+                  <MenuIcon/>
+                </IconButton>
+                <Typography variant="title" color="inherit" noWrap="noWrap">
+                  Responsive drawer
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <Hidden mdUp="mdUp">
+              <Drawer variant="temporary" anchor={theme.direction === 'rtl'
+                  ? 'right'
+                  : 'left'} open={this.state.mobileOpen} classes={{
+                  paper: classes.drawerPaper
+                }} onClose={this.handleDrawerToggle} ModalProps={{
+                  keepMounted: true, // Better open performance on mobile.
+                }}>
+                {/* {drawer} */}
+                <ul>
+                  <li>hello</li>
+                </ul>
+              </Drawer>
+            </Hidden>
+            <Hidden smDown="smDown" implementation="css">
+              <Drawer variant="permanent" open="open" classes={{
+                  paper: classes.drawerPaper
+                }}>
+                {/* {drawer} */}
+                <ul>
+                  <li>hello</li>
+                </ul>
+              </Drawer>
+            </Hidden>
+            <main className={classes.content}>
+              <Typography noWrap="noWrap">{'You think water moves fast? You should see ice.'}</Typography>
+            </main>
           </div>
-        </MuiThemeProvider>
-        
-      </Router>
-      
-      // <div className="App">
-      //   <Header {...this.state} pageChange={this.onPageChange} />
-      //   <Route exact path="/" render={() => <Dashboard {...this.state} onPageDetailsChange={this.onPageDetailsChange} ></Dashboard>}/>
-      //   <Route exact path="/work-orders" component={() => <WorkOrders {...this.state}/>}/>
-      //   <Route exact path="/events" component={() => <Events {...this.state} pageChange={this.onPageChange}/>}/>
-      //   <Route exact path="/event/:id" component={() => <Events {...this.state} pageChange={this.onPageChange}/>}/>
-      // </div>
-    );
+        </div>
+
+        {/* <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit
+            <code>src/App.js</code>
+            and save to reload.
+          </p>
+          <Button variant="raised" color="primary">
+            Primary
+          </Button>
+        </div> */}
+      </MuiThemeProvider>
+
+    </Router>)
+
+    // <div className="App">
+    //   <Header {...this.state} pageChange={this.onPageChange} />
+    //   <Route exact path="/" render={() => <Dashboard {...this.state} onPageDetailsChange={this.onPageDetailsChange} ></Dashboard>}/>
+    //   <Route exact path="/work-orders" component={() => <WorkOrders {...this.state}/>}/>
+    //   <Route exact path="/events" component={() => <Events {...this.state} pageChange={this.onPageChange}/>}/>
+    //   <Route exact path="/event/:id" component={() => <Events {...this.state} pageChange={this.onPageChange}/>}/>
+    // </div>);
   }
 }
 
-export default App;
+export default withStyles(styles, {withTheme: true})(App);
