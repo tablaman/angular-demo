cff0506d79d3fe5c78b93bba481e47c5ef7d4023
tablaman <tablaman@gmail.com>
2018-06-04 12:13:27 +0800

    Completed Redux app with the splitting of components
    Signed-off-by: tablaman <tablaman@gmail.com>

--- redux-tyler-mcginnis/goals-todos/src/components/App.js
+++ redux-tyler-mcginnis/goals-todos/src/components/App.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react';
+ import React from "react";
+ import List from "./List";
+ import ConnectedTodos from './Todos';
+ import ConnectedGoals from './Goals';
+ import {
+   connect
+ } from 'react-redux';
 
-class App extends Component {
+ import {
+   handleInitialData
+ } from '../actions/shared';
+
+class App extends React.Component {
+  componentDidMount () {
+    const { dispatch } = this.props;
+    dispatch(handleInitialData())
+  }
   render() {
+    const { loading } = this.props;
+
+    if (loading === true) {
+      return <h3>Loading</h3>
+    }
+
     return (
-      <div className="App">
-        hello World!
+      <div>
+        <ConnectedTodos />
+        <ConnectedGoals />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+// connect: Render any component, passing that component any data 
+// it needs from the store
+// we have a function that passes data that App needs
+export default connect((state) => ({
+  loading: state.loading
+}))(App)
