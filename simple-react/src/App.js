41a0cac5a7efe60bb59d3af748d36323e085e925
Mili Wijeratne <tablaman@gmail.com>
2018-06-08 17:35:38 +0800

    simple-react boilerplate

--- simple-react/src/App.js
+++ simple-react/src/App.js
@@ -5,26 +5,35 @@ import Routes from './Routes';
 
 import reactLogo from './assets/React-icon.png';
 
-const App = () => (
-  <BrowserRouter>
-    <main className="container">
-      <div>
-        <h1>hello world!</h1>
-        <img className="container__image" alt="react logo" src={reactLogo} />
-        <p>If you see this everything is working!</p>
-      </div>
-      <ul className="left">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-      <Routes />
-    </main>
-  </BrowserRouter>
-);
+class App extends React.Component {
+  componentWillMount() {
+    fetch('http://localhost:8080/ping').then(res => console.log(res))
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <main className="container">
+          <div>
+            <h1>hello world!</h1>
+            <img className="container__image" alt="react logo" src={reactLogo} />
+            <p>If you see this everything is working!</p>
+          </div>
+          <ul className="left">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+          <Routes />
+        </main>
+      </BrowserRouter>
+    );
+  }
+}
 
 export default App;
 
