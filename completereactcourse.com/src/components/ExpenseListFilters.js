f2bb66e395afbe82bd7a6647ec714f74f172f5b2
tablaman <tablaman@gmail.com>
2018-06-11 23:31:17 +0800

    Completed Redux impl

--- completereactcourse.com/src/components/ExpenseListFilters.js
+++ completereactcourse.com/src/components/ExpenseListFilters.js
@@ -1,33 +1,62 @@
 import React from "react";
 import { connect } from 'react-redux';
-import { setTextFilter, sortByAmount, sortByDate } from "../actions/filters";
+import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from "../actions/filters";
+import "react-dates/initialize"; // hack needed to get react-dates working
+import { DateRangePicker } from 'react-dates';
 
-
-const ExpenseListFilters = (props) => (
-  <div>
-    <input type="text" 
-      value={props.filters.text}
-      onChange={(e) => {
-        props.dispatch(setTextFilter({text: e.target.value}));
-      }} />
-      <select 
-        value={props.filters.sortBy}
-        onChange={(e) => {
-          switch(e.target.value) {
-            case 'date':
-              return props.dispatch(sortByDate())
-            case 'amount':
-              return props.dispatch(sortByAmount())
-            default:
-              break;
-          }
-        
-      }}>
-        <option value="date">Date</option>
-        <option value="amount">Amount</option>
-      </select>
-  </div>
-);
+class ExpenseListFilters extends React.Component {
+  state = {
+    calendarFocused: null
+  };
+  onDatesChange = ({ startDate, endDate }) => {
+    this.props.dispatch(setStartDate(startDate));
+    this.props.dispatch(setEndDate(endDate));
+  };
+  onFocusChange = (calendarFocused) => {
+    this.setState(() => ({ calendarFocused }))
+  }
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.props.filters.text}
+          onChange={e => {
+            this.props.dispatch(setTextFilter({ text: e.target.value }));
+          }}
+        />
+        <select
+          value={this.props.filters.sortBy}
+          onChange={e => {
+            switch (e.target.value) {
+              case "date":
+                return this.props.dispatch(sortByDate());
+              case "amount":
+                return this.props.dispatch(sortByAmount());
+              default:
+                break;
+            }
+          }}
+        >
+          <option value="date">Date</option>
+          <option value="amount">Amount</option>
+        </select>
+        <DateRangePicker
+          startDateId='startDateId'
+          endDateId='endDateId'
+          startDate={this.props.filters.startDate}
+          endDate={this.props.filters.endDate}
+          onDatesChange={this.onDatesChange}
+          focusedInput={this.state.calendarFocused}
+          onFocusChange={this.onFocusChange}
+          showClearDates={true}
+          numberOfMonths={1}
+          isOutsideRange={() => false}
+        />
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
--- completereactcourse.com/src/selectors/expenses.js
+++ completereactcourse.com/src/selectors/expenses.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 // Get visible expenses
 const getVisibleExpenses = (expenses, {
   text,
@@ -6,8 +7,12 @@ const getVisibleExpenses = (expenses, {
   endDate
 }) => {
   return expenses.filter((expense) => {
-    const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-    const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+    const createdAtMoment = moment(expense.createdAt)
+    const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day'): true
+    const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
+    
+    // const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+    // const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
     const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
 
